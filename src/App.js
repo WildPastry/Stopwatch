6797e47bcdb2d0b86c70cac057a15044feb908cc
WildPastry <mykdsn@gmail.com>
2019-03-28 18:25:56 +1300

    Update stopwatch

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ class App extends Component {
         timerOn: false,
         timerText: 0,
         buttonText: 'START',
-        buttonClass: 'btn btn-warning'
+        buttonClass: 'btn btn-primary'
       }
 
       this.toggleTimer = this.toggleTimer.bind(this);
+      this.resetWatch = this.resetWatch.bind(this);
+      
 }
 
 // FUNCTIONS
@@ -24,12 +26,22 @@ toggleTimer(){
     timerInt = setInterval(
       () => this.tick(), 1000
     );
-
+    this.setState({
+      timerOn: true,
+      buttonText: 'Stop Timer',
+      buttonClass: 'btn btn-warning'
+  })
   } else {
-  console.log('turned timer off');
+    clearInterval(timerInt);
+    this.setState({
+        timerOn: false,
+        buttonText: 'Continue Timer',
+        buttonClass: 'btn btn-primary'
+    })
   }
 }
 
+// FUNCTIONS
 tick(){
   var newTime = this.state.timerText +1;
   this.setState({
@@ -37,13 +49,25 @@ tick(){
   })
 }
 
+resetWatch(){
+  this.setState({
+    timerText: 0,
+      buttonText: 'Start Stop Watch',
+      buttonClass: 'btn btn-primary'
+  })
+}
+
   // RENDER
   render() {
 
     let {buttonText, buttonClass} = this.state;
+    let button;
+    if( (this.state.timerOn === false) && (this.state.timerText > 0) ){
+        button = <button className="btn btn-danger" onClick={this.resetWatch}>Reset Stop Watch</button>;
+    }
 
     return (
-      <div className="d-flex align-items-center h-100 bg-danger">
+      <div className="d-flex align-items-center h-100 bg-dark">
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-6">
@@ -51,6 +75,7 @@ tick(){
                 <h1 className="display-4">Stopwatch</h1>
                 <h2 className="display-2">{this.state.timerText}</h2>
                 <button className={buttonClass} onClick={this.toggleTimer}>{buttonText}</button>
+                {button}
               </div>
             </div>
           </div>
