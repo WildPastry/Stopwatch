43737e6e93775972d016e8b0a32ed160efa2fe1e
WildPastry <mykdsn@gmail.com>
2022-10-09 14:46:11 +1300

    Upgrade to TS. Fix linting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-let timerInt;
+let timerInt: string | number | NodeJS.Timer | undefined;
 
-class App extends Component {
+class App extends React.Component<{},
+{ timerOn: boolean,
+  timerText: number,
+  buttonText: string,
+  buttonClass: string
+}> {
   // CONSTRUCTOR
-  constructor() {
-    super();
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
     this.state = {
       timerOn: false,
       timerText: 0,
       buttonText: 'START',
-      buttonClass: 'btn btn-primary'
+      buttonClass: 'btn btn-info'
     };
     this.toggleTimer = this.toggleTimer.bind(this);
     this.resetWatch = this.resetWatch.bind(this);
@@ -22,20 +27,19 @@ class App extends Component {
       timerInt = setInterval(() => this.tick(), 1000);
       this.setState({
         timerOn: true,
-        buttonText: 'Stop Timer',
+        buttonText: 'STOP',
         buttonClass: 'btn btn-warning'
       });
     } else {
       clearInterval(timerInt);
       this.setState({
         timerOn: false,
-        buttonText: 'Continue Timer',
-        buttonClass: 'btn btn-primary'
+        buttonText: 'CONTINUE',
+        buttonClass: 'btn btn-info'
       });
     }
   }
 
-  // FUNCTIONS
   tick() {
     const newTime = this.state.timerText + 1;
     this.setState({
@@ -46,8 +50,8 @@ class App extends Component {
   resetWatch() {
     this.setState({
       timerText: 0,
-      buttonText: 'Start Stop Watch',
-      buttonClass: 'btn btn-primary'
+      buttonText: 'START',
+      buttonClass: 'btn btn-info'
     });
   }
 
@@ -55,9 +59,9 @@ class App extends Component {
   render() {
 
     const { buttonText, buttonClass } = this.state;
-    let button;
+    let button: string | number | boolean | React.ReactFragment | JSX.Element | null | undefined;
     if ((this.state.timerOn === false) && (this.state.timerText > 0)) {
-      button = <button className='btn btn-danger' onClick={this.resetWatch}>Reset Stop Watch</button>;
+      button = <button className='btn btn-danger' onClick={this.resetWatch}>RESET</button>;
     }
 
     return (
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React, { StrictMode } from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
